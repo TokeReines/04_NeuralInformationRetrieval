c133bd64276b8b8097a5678f66a609c06f835ea6
Toke Reines <toke.reines@gmail.com>
2022-05-04 13:26:10 +0200

    initial

---
+++ indexer.py
@@ -0,0 +1,14 @@
+import pyterrier as pt
+import pandas as pd
+
+if __name__ == '__main__':
+    pt.init()
+    docs_df = pd.read_csv('data/lab_docs.csv', dtype=str)
+    print(docs_df.shape)
+    print(docs_df.head())
+
+    indexer = pt.DFIndexer("./indexes/default", overwrite=True)
+    index_ref = indexer.index(docs_df["text"], docs_df["docno"])
+    print(index_ref.toString())
+    index = pt.IndexFactory.of(index_ref)
+    a = 2
